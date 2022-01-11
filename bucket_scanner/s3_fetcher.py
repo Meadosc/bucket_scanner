45e80f178ad15fac4ea09f5db94dc55b1b0be849
meadosc <meadosc@gmail.com>
2022-01-11 12:32:50 -0800

    update s3 fetcher

--- bucket_scanner/s3_fetcher.py
+++ bucket_scanner/s3_fetcher.py
@@ -5,9 +5,18 @@ import gzip
 import json
 
 import boto3
+import yaml
 
-from helper import Constants
 
+class Constants:
+	def __init__(self):
+		with open('constants.yaml', 'r') as f:
+			constants = yaml.safe_load(f)
+		self.constants = constants
+		self.prefix = constants['prefix']
+		self.prefix_start = constants['prefix_start']
+		self.bucket = constants['s3_bucket_name']
+	
 
 def get_s3_data_chunk():	
 	"""	Get s3 data chunk from prefix defined in constants.yaml """
